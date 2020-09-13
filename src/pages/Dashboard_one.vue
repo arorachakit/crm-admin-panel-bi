4e54b14807b119edf9607d6179ceeb7c60ac36de
Chakit Arora <arorachakit@gmail.com>
2020-09-14 01:54:47 +0530

    initial dashboard setup

---
+++ src/pages/Dashboard_one.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-page>
+    <div class="row q-col-gutter-sm q-ma-sm q-pr-sm">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+        <q-card class="bg-green">
+          <q-card-section>
+            <div class="row">
+              <div class="col-8">
+                <p class="text-h4">$3,123</p>
+                <p>Total Sales</p>
+              </div>
+              <div class="offset-2 col-2">
+                <q-icon class="q-mt-md" name="trending_up" size="xl"/>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12">
+        <q-card>
+          <q-card-section>
+            <div class="flex column flex-center">
+                <p class="text-h4" style="color: green">3700</p>
+                <p>Total Opportunities</p>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12">
+        <q-card>
+          <q-card-section>
+            <div class="flex column flex-center">
+
+                <p class="text-h4" style="color: green">3700</p>
+                <p>Total Opportunities</p>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+        <q-card class="bg-green">
+          <q-card-section>
+            <div class="row">
+              <div class="col-8">
+                <p class="text-h4">$3,123</p>
+                <p>Total Sales</p>
+              </div>
+              <div class="offset-2 col-2">
+                <q-icon class="q-mt-md" name="trending_up" size="xl"/>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+            <div class="col-lg-2 col-md-6 col-sm-6 col-xs-12">
+        <q-card>
+          <q-card-section>
+            <div class="flex column flex-center">
+
+                <p class="text-h4" style="color: green">3700</p>
+                <p>Total Opportunities</p>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+<script>
+import IEcharts from "vue-echarts-v3/src/full.js";
+export default {
+
+}
+</script>
+<style lang="stylus" scoped>
+  .echarts{
+    height : 200px
+  }
+</style>
