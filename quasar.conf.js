942f18c74e5a0530784c91198abfb361f003e11b
Chakit Arora <arorachakit@gmail.com>
2020-09-13 19:11:50 +0530

    add login

--- quasar.conf.js
+++ quasar.conf.js
@@ -98,7 +98,9 @@ cfg.module.rules.push({
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fullscreen bg-grey-4">
     <div class="flex flex-center" style="height: 100%; width: 100%">
-      <q-card style="width: 80%; height: 60%" :style="$q.platform.is.mobile? 'width: 85%; height: 65%': 'width: 50%; height: 60%'">
+      <q-card :style="$q.platform.is.mobile? 'width: 85%; height: 65%': 'width: 50%; height: 60%'">
         <q-card-section
           :horizontal="!$q.platform.is.mobile"
           style="height: 100%"
@@ -13,37 +13,45 @@
           />
           <q-card-section class="col-5">
             <div class="" style="width: 100%" :class="!$q.platform.is.mobile? 'q-mt-xl q-pt-xl': 'q-mt-sm'">
-              <h6 text-h6 class="q-pl-xs q-pb-sm q-my-xs">Login to Admin Panel</h6>
+              <h6 text-h6 class="q-pb-sm q-my-xs">Login to Admin Panel</h6>
               <q-form
+                ref="loginForm"
                 class="q-gutter-none"
               >
                 <q-input
-                  filled
-
+                  dense
+                  clearable
                   v-model="username"
                   label="Your name *"
                   lazy-rules
                   :rules="[ val => val && val.length > 0 || 'Please type something']"
                 />
                 <q-input
-                  filled
-
-                  type="password"
+                  dense
                   label="Password *"
                   v-model="password"
                   lazy-rules
+                  :type="isPwd ? 'password' : 'text'"
                   :rules="[
                     val => val !== null && val !== '' || 'Please type something',
                   ]"
-                />
+                >
+                  <template v-slot:append>
+                    <q-icon
+                      :name="isPwd ? 'visibility_off' : 'visibility'"
+                      class="cursor-pointer"
+                      @click="isPwd = !isPwd"
+                    />
+                  </template>
+                </q-input>
                 <div>
-                  <q-btn class="full-width" label="Login" type="submit" flat color="cyan"/>
+                  <q-btn class="full-width" label="Login" type="submit" @click="submit" color="green"/>
                   <div class="row q-mt-lg">
                     <div class="col-5">
-                      <q-btn class="full-width" icon="eva-google" color="grey-5" type="submit"/>
+                      <q-btn class="full-width" flat icon="eva-google" color="grey-5" type="submit" to="/dashboard"/>
                     </div>
                     <div class="offset-2 col-5">
-                      <q-btn class="full-width" icon="eva-facebook" color="blue" type="submit"/>
+                      <q-btn class="full-width" flat icon="eva-facebook" color="blue" type="submit" to="/dashboard"/>
                     </div>
                   </div>
                 </div>
@@ -62,7 +70,18 @@ export default {
   data(){
     return {
       username: 'chakit',
-      password: 'admin'
+      password: 'admin',
+      isPwd: true
+    }
+  },
+  methods: {
+    submit(){
+      if(!this.$refs.loginForm.hasError){
+        this.$router.push('/dashboard');
+        this.$q.notify({
+          message: 'Successfully Logged In'
+        })
+      }
     }
   }
 };
